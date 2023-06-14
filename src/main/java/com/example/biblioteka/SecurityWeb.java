6283b66d9af4ca378ed477f8c8f9605005ea90ef
BartekLabrync <barteklabrync7@gmail.com>
2023-06-14 17:40:53 +0200

    14.06.2023

---
+++ src/main/java/com/example/biblioteka/SecurityWeb.java
@@ -0,0 +1,11 @@
+package com.example.biblioteka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+public class SecurityWeb {
+    public static void main(String[] args) {
+        SpringApplication.run(WebSecurityConfig.class, args);
+    }
+}
